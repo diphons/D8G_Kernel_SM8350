d7460af6af397243f440baaeea266cffa4f4eaab
diphons <diphons@gmail.com>
2023-07-27 07:22:04 +0700

    Import AnyKernel3 with inline from https://github.com/diphons/D8G_Kernel_Marble

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,39 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=D8G Kernel by diphons
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=vili
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+
+## boot shell variables
+block=boot
+is_slot_device=1
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+# Begin Ramdisk Changes
+. /tmp/anykernel/tools/install.sh;
+
+write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
